9d87cac4d1e3dd8362f253f584e699a4dadecbf8
eungyu <beg4660@naver.com>
2025-06-09 21:41:35 +0900

    ui: 네비바 추가, 폰트변경

--- src/components/layout/mobile-view.tsx
+++ src/components/layout/mobile-view.tsx
@@ -3,7 +3,7 @@ import { NavBar } from "../nav-bar/NavBar";
 
 export const MobileView: React.FC = () => {
   return (
-    <div className="min-h-screen bg-base-300 font-GmarketSans">
+    <div className="min-h-screen bg-base-300 font-SpoqaHanSansNeo">
       <div className="px-8 py-9.5 mx-auto max-w-md min-h-screen bg-main-white-500 relative">
         <Outlet />
         <NavBar />
--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import gray_back from "../../assets/image/navbar/gray_bank.png";
 import gray_home from "../../assets/image/navbar/gray_home.png";
 import gray_chart from "../../assets/image/navbar/gray_chart.png";
@@ -11,14 +12,41 @@ import green_store from "../../assets/image/navbar/green_store.png";
 import green_setup from "../../assets/image/navbar/green_setup.png";
 
 export const NavBar: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-    <div className="fixed bottom-0 left-0 right-0 mx-auto max-w-md flex justify-between mb-8 bg-gray-100 rounded-xl p-1">
-      <div className="flex justify-between w-full px-10">
-        <img src={gray_back} alt="" className="w-6 h-6 object-contain" />
-        <img src={gray_home} alt="" className="w-6 h-6 object-contain" />
-        <img src={gray_chart} alt="" className="w-6 h-6 object-contain" />
-        <img src={gray_store} alt="" className="w-6 h-6 object-contain" />
-        <img src={gray_setup} alt="" className="w-6 h-6 object-contain" />
+    <div
+      className={`fixed bottom-0 left-0 right-0 mx-auto max-w-md mb-8 transition-transform duration-300 ${
+        isVisible ? "translate-y-0" : "translate-y-[200%]"
+      }`}
+    >
+      <div className="flex justify-between bg-black/20 rounded-full w-[calc(100%-4rem)] mx-auto px-8 py-3">
+        <img src={gray_back} alt="" className="w-8 h-8 object-contain" />
+        <img src={gray_home} alt="" className="w-8 h-8 object-contain" />
+        <img src={gray_chart} alt="" className="w-8 h-8 object-contain" />
+        <img src={gray_store} alt="" className="w-8 h-8 object-contain" />
+        <img src={gray_setup} alt="" className="w-8 h-8 object-contain" />
       </div>
     </div>
   );
